282e137e0c5b667d44afb8f74d590f7e662e0bbb
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-08 14:09:23 +0200

    added a script to compile sphinx_docs automatically

---
+++ build_docs.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+# A little script to watch for changes in the document folder
+# and recompile the documentation if there is a change.
+
+SOURCE_DIR=$HOME/Documents/sphinx_docs/source
+MAKE_DIR=$HOME/Documents/sphinx_docs/
+
+compile_docs ()
+{
+    cur_dir=$(pwd)
+    cd $MAKE_DIR
+    make html
+    cd $cur_dir
+}
+
+while true; do
+    inotifywait --recursive --quiet -q --event modify $SOURCE_DIR &&\
+    compile_docs
+done
+
